67a0a2fbb622770e49c64b68bb5f4bc95140c255
Erisha Roach <forever.erisha@gmail.com>
2023-12-07 23:55:30 -0500

    part 2 done working on part 3

---
+++ script.js
@@ -0,0 +1,51 @@
+const Csv = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26"
+let firstColumn = Csv.indexOf('\n')
+let firstRow = Csv.substring(0, firstColumn)
+let column = firstRow.split(',').length;
+console.log(column);
+
+let  currentCell = "";
+let currentRow = [];
+let columnNum = 0;
+let allData = [];
+
+for(let i = 0; i < Csv.length; i++){
+    let char = Csv[i];
+    let comma = char == ',';
+    let newLine = char === '\n';
+
+    if (comma || newLine){
+        currentRow.push(currentCell);
+        currentCell = "";
+        columnNum++;
+
+    if (columnNum === column || newLine ){
+        allData.push(currentRow);
+        currentRow = [];// starts for loop over
+        columnNum = 0;
+    }
+
+    if(newLine && i === firstColumn){
+        firstColumn = -1; 
+    }
+
+    } else{
+        currentCell += char
+    }
+}
+if (currentRow.length > 0 || currentCell){
+    if (currentCell){
+        currentRow.push(currentCell)
+    }
+    allData.push(currentRow)
+}
+console.log(allData);
+
+let allData2 = [
+    ["ID", "Name", "Occupation", "Age"],
+    ["42", "Bruce", "Knight", "41"],
+    ["57", "Bob", "Fry Cook", "19"],
+    ["63", "Blaine", "Quiz Master", "58"],
+    ["98", "Bill", "Doctor’s Assistant", "26"],
+  ]
+  
